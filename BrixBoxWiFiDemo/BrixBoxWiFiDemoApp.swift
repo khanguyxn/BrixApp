699879aef462c7744d26fb017d12c2112bba69aa
khanguyxn <mknguyen35@gmail.com>
2025-12-20 10:20:22 -0800

    Initial Commit

---
+++ BrixBoxWiFiDemo/BrixBoxWiFiDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrixBoxWiFiDemoApp.swift
+//  BrixBoxWiFiDemo
+//
+//  Created by Khang Nguyen on 12/20/25.
+//
+
+import SwiftUI
+
+@main
+struct BrixBoxWiFiDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrixBoxWiFiDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BrixBoxWiFiDemo
+//
+//  Created by Khang Nguyen on 12/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
